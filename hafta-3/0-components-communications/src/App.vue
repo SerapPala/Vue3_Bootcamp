e95bd5e643c9a158d5477aba0e0c502694dfecc0
Serap Pala <serap68040@gmail.com>
2022-07-24 21:30:41 +0300

    0-components-communications

---
+++ hafta-3/0-components-communications/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <h3>containerim</h3>
+    
+    <UserSection/>
+
+
+  </div>
+</template>
+
+<script>
+import UserSection from "@/components/UserSection";
+export default {
+  components: {
+    UserSection
+  },
+  data() {
+    return {
+      provideData: {
+        userList: ["serap", "sinem", "selin"]
+      }
+
+    }
+  },
+methods:{
+    newItem(item){
+      this.provideData.userList.push(item);
+      
+    }
+},
+ provide(){
+  return{
+    userList : this.provideData.userList,
+    newItem : this.newItem
+  };
+ }
+}
+
+
+
+</script>
+
+
---
+++ hafta-3/0-components-communications/src/components/UserSection.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <ListSection />
+    <AddSection />
+
+</template>
+
+<script>
+import ListSection from '@/components/ListSection.vue';
+import AddSection from '@/components/AddSection.vue';
+
+export default {
+    // props: ["userList"],
+    // emits: ['new-item'],
+
+    components: {
+        ListSection,
+        AddSection
+    }
+
+};
+
+</script>

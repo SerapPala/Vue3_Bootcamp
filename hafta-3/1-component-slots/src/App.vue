7c225357f24eb9f47ff40461038f87c3e449a812
Serap Pala <serap68040@gmail.com>
2022-07-27 18:54:22 +0300

    slot completed

---
+++ hafta-3/1-component-slots/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <!--  <ModalComponent :onSave="onSave" :title="'Modal Title'" :content="content" /> -->
+
+  <ModalComponent>
+
+    <template #title>
+      <h3>slot ile gelen title bilgisi</h3>
+    </template>
+
+ <!--  <template #content>
+    <p class="text-green">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus nobis laboriosam alias voluptates aliquam quae id ducimus inventore excepturi. Impedit illum et laudantium fugiat qui deleniti animi aliquid, cumque facere!</p>
+    </template> -->
+
+     <template v-slot:content>
+    <p class="text-green">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus nobis laboriosam alias voluptates aliquam quae id ducimus inventore excepturi. Impedit illum et laudantium fugiat qui deleniti animi aliquid, cumque facere!</p>
+    </template>
+
+
+
+    <template #default>
+      Bu default olarak gösterilecek.
+    </template>
+
+  </ModalComponent>
+
+
+</template>
+
+<script>
+import ModalComponent from "@/components/ModalComponent"
+export default {
+  components: {
+    ModalComponent
+  },
+  data() {
+    return {
+      content: `<h3 class="text-red"></h3>`
+    }
+  },
+  methods: {
+    onSave() {
+      alert("saved");
+    }
+  }
+}
+</script>
+
---
+++ hafta-3/1-component-slots/src/components/ModalComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+
+        <div class="header">
+
+            <slot name="title"></slot>
+
+        </div>
+
+        <slot></slot>
+        
+        <hr />
+
+        <div class="content my-2">
+            <slot name="content"></slot>
+        </div>
+
+
+        <div class="footer text-right">
+            <button class="mr-2 red">Kapat</button>
+            <button @click="onSave" class="green">Kaydet</button>
+        </div>
+
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: ["title", "content", "onSave"]
+}
+</script>
